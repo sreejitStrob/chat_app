b050bfd065adadf4b41b9d5e7f45040d85219b48
zerocoolvishal <zerocoolvishal@gmail.com>
2020-01-30 10:57:08 +0530

    send entire $_POST

--- chat.php
+++ chat.php
@@ -1,22 +1,20 @@
 <?php
-  require __DIR__ . '/vendor/autoload.php';
-if(isset($_POST['name'])){
-  $options = array(
-    'cluster' => 'ap2',
-    'useTLS' => true
-  );
-  $pusher = new Pusher\Pusher(
-    '7455a3c52b935d083656',
-    '11f660f05efe0b40d313',
-    '938995',
-    $options
-  );
 
-  $data = json_encode([
-    'message' => $_POST['message'],
-    'name' => $_POST['name'],
-    'color' => $_POST['color']
-  ]);
-  $pusher->trigger('my-channel', 'my-event', $data);
-}
+  require __DIR__ . '/vendor/autoload.php';
+
+  if(isset($_POST['name'])){
+    $options = array(
+      'cluster' => 'ap2',
+      'useTLS' => true
+    );
+    $pusher = new Pusher\Pusher(
+      '7455a3c52b935d083656',
+      '11f660f05efe0b40d313',
+      '938995',
+      $options
+    );
+    
+    $data = json_encode($_POST);
+    $pusher->trigger('my-channel', 'my-event', $data);
+  }
 ?>
